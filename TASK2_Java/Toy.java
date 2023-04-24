d2ab1850767bf508172076afa62b6b7b5bcbb532
Ruslan Sakaev <hardist@mail.ru>
2023-04-25 00:46:56 +0300

    Создал 3 класса

---
+++ TASK2_Java/Toy.java
@@ -0,0 +1,46 @@
+public class Toy {
+    private int id;
+    private String name;
+    private int quantity;
+    private int weight;
+
+    public Toy(int id, String name, int quantity, int weight) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.weight = weight;
+    }
+
+    // геттеры и сеттеры для всех полей
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
