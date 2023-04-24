9423bc83bad4c161d77af9267f6102fa15a938cb
Ruslan Sakaev <hardist@mail.ru>
2023-04-25 01:02:22 +0300

    Описал класс магазина игрушек и дополнил README

---
+++ TASK2_Java/ToyStore.java
@@ -0,0 +1,51 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ToyStore {
+    private List<Toy> toys = new ArrayList<>();
+
+    public void addToy(Toy toy) {
+        toys.add(toy);
+    }
+
+    public void updateToyWeight(int id, int weight) {
+        for (Toy toy : toys) {
+            if (toy.getId() == id) {
+                toy.setWeight(weight);
+                break;
+            }
+        }
+    }
+
+    public Toy getPrizeToy() {
+        Random rand = new Random();
+        int totalWeight = 0;
+        for (Toy toy : toys) {
+            totalWeight += toy.getWeight();
+        }
+        int randWeight = rand.nextInt(totalWeight);
+        int currentWeight = 0;
+        for (Toy toy : toys) {
+            currentWeight += toy.getWeight();
+            if (currentWeight > randWeight) {
+                toy.setQuantity(toy.getQuantity() - 1);
+                writeToFile(toy.getName());
+                return toy;
+            }
+        }
+        return null;
+    }
+
+    private void writeToFile(String toyName) {
+        try {
+            FileWriter writer = new FileWriter("prizes.txt", true);
+            writer.write(toyName + "\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
